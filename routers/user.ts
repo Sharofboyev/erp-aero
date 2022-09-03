6d7629f21eb1ab59475cd393590e9ddf9840fd6d
Sarvar <sharofboyev.sarvar@gmail.com>
2022-09-03 14:24:50 +0500

    Bug fix

--- routers/user.ts
+++ routers/user.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import { giveID, logout, signIn, refreshToken, signUp } from "../controllers/user";
-import {auth, authRefreshToken } from "../middlewares/auth";
+import {auth } from "../middlewares/auth";
 
 const router = express.Router();
 
 router.get("/info", auth, giveID);
-router.get("/logout", auth, authRefreshToken, logout);
+router.get("/logout", auth, logout);
 router.post("/signin", signIn);
-router.post("/signin/new_token", authRefreshToken, refreshToken);
+router.post("/signin/new_token", refreshToken);
 router.post("/signup", signUp);
 
 export default router;
